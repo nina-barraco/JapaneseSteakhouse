0f6dc4c46d6e5601ee440d1f9536c514d1b6d01c
fearthebeard88 <tferris88@gmail.com>
2019-02-16 14:21:46 -0500

    finished reservation schema for database

--- Model/Reservtion.js
+++ Model/Reservtion.js
@@ -5,5 +5,23 @@ const Reservation = new Schema({
     firstName: {
         type: String,
         required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
     }
-})
+});
+
+module.exports = mongoose.model('Reservation', Reservation);
